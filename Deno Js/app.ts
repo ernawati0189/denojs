c7c52c0f407dfed041177e1d77ebcf048267f556
ernawati0189 <74448260+ernawati0189@users.noreply.github.com>
2020-12-03 20:03:51 +0700

    Add files via upload

---
+++ Deno Js/app.ts
@@ -0,0 +1,20 @@
+import {
+  Application,
+  RouterContext,
+  send,
+} from "https://deno.land/x/oak/mod.ts";
+import router from "./rute.ts";
+
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(async (context) => {
+  await send(context, context.request.url.pathname, {
+    root: `${Deno.cwd()}`,
+  });
+});
+
+console.log("Service port 8000");
+await app.listen({ port: 8000 });
